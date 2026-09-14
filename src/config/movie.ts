b82273f16ddae7206419b9951eb3625e8a147bdd
Tio Ramadhan <tio.ramadhan@cimbniaga.co.id>
2026-09-14 23:00:01 +0700

    feat: init dependency and setup api path of tmdb

---
+++ src/config/movie.ts
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "https://api.themoviedb.org";
+
+export enum MOVIE {
+	NOW_PLAYING = "/3/movie/now_playing",
+	POPULAR = "/3/movie/popular",
+	TOP_RATED = "/3/movie/top_rated",
+	UPCOMING = "/3/movie/upcoming",
+	DETAILS = "/3/movie",
+	SEARCH = "/3/search/movie",
+}
